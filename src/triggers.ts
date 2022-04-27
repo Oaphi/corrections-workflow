1f5cb952a2366ac5938b7e140fd42cdf270737d3
Oleg Valter <o.a.philippov@gmail.com>
2022-04-27 07:07:58 +0300

    abstracted trigger creation

--- src/triggers.ts
+++ src/triggers.ts
@@ -1,23 +1,34 @@
 /**
- * @summary installs the time-based trigger
+ * @summary installs a time-based trigger
+ * @param name trigger name
+ * @param callback function to call
+ * @param minutes interval (in minutes)
  */
-const installTrigger = () => {
-    const key = "new_items_trgger";
-
+const installTrigger = (name: string, callback: Function, minutes: number) => {
     const store = PropertiesService.getScriptProperties();
 
-    const existing = store.getProperty(key);
+    const existing = store.getProperty(name);
     if (existing) {
-        console.log("new items trigger already exists");
+        console.log(`[${name}] trigger already exists`);
         return;
     }
 
     const trigger = ScriptApp
-        .newTrigger(processNewItems.name)
+        .newTrigger(callback.name)
         .timeBased()
-        .everyMinutes(5)
+        .everyMinutes(minutes)
         .create();
 
     const triggerId = trigger.getUniqueId();
-    store.setProperty(key, triggerId);
+    store.setProperty(name, triggerId);
 };
+
+/**
+ * @summary installs trigger for new items processing
+ */
+const installNewItemsTrigger = () => installTrigger("new_items_trgger", processNewItems, 5);
+
+/**
+ * @summary installs trigger for ready items processing
+ */
+const installReadyItemsTrigger = () => installTrigger("ready_items_trigger", processReadyItems, 30);
